da37ae80f0b7ef9b1b37fa3f92f256f104e95e61
panpanWang <wangpanpan@huayugroup360.com>
2017-07-20 14:27:55 +0800

    瞎说的

--- app/src/main/java/com/huayu/android/downloaddogapplication/MainActivity.java
+++ app/src/main/java/com/huayu/android/downloaddogapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.huayu.android.downloaddogapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState != null) {
-            //lulllaa
+            //lulllaa范晓飞是帅哥哈
         }
+
     }
 }
